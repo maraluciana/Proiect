06b0066121d1e57c0d7469fe96da755c02dfedf4
unknown <maraa.lucianaa@yahoo.com>
2024-11-04 22:41:58 +0200

    initial commit

---
+++ src/main/java/org/example/LinkedList.java
@@ -0,0 +1,74 @@
+package org.example;
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private int size = 0;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void add(T element) {
+        Node<T> newNode = new Node<>(element);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public boolean remove(T element) {
+        if (head == null) {
+            return false;
+        }
+
+        if (head.data.equals(element)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+
+        Node<T> current = head;
+        while (current.next != null && !current.next.data.equals(element)) {
+            current = current.next;
+        }
+
+        if (current.next != null) {
+            current.next = current.next.next;
+            size--;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean contains(T element) {
+        Node<T> current = head;
+        while (current != null) {
+            if (current.data.equals(element)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
